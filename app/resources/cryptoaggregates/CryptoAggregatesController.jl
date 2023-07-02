8f10c3366582e9951878baa8c89a5b45c6f74df8
Renruize12306 <renruize1998@gmail.com>
2023-07-02 17:56:12 -0400

    finish the backend set up

--- app/resources/cryptoaggregates/CryptoAggregatesController.jl
+++ app/resources/cryptoaggregates/CryptoAggregatesController.jl
@@ -16,21 +16,18 @@ end
 function initiate_subscription()
   # For now, we only allows subscribe one ticker at a time
   task = subscription_helper()
-  # @info "The subscription_helper has been called"
-  str = params(:ticker_to_subscribe) * "%%%" * params(:ticker_to_unsubscribe) * "%%%" * string(task) * "is running"
+  str = params(:ticker_to_subscribe) * "%%%" * params(:ticker_to_unsubscribe) * "%%%" * string(task) * " is running"
 
   Json.json(Dict("crypto_aggregates" => str))
 end
 
 # ====================WSS API Begins====================
 include("../../../constant.jl")
-using JSON, WebSockets
+using JSON, HTTP.WebSockets
 function subscription_helper()
   json_data = JSON.parse(String(event_data))
-  # @info "json_data has been parsed"
   task = @async open_websocket(json_data["input_json"])
   # open_websocket(json_data["input_json"])
-  # @info "The async task has been called"
   return task
 end
 
@@ -38,37 +35,64 @@ function open_websocket(arr)
   if length(arr) > 1
     uri = "wss://socket.polygon.io/crypto"
     WebSockets.open(uri) do ws
-      if isopen(ws)
-        write(ws, JSON.json(arr[1]))
-      end
+      WebSockets.send(ws, JSON.json(arr[1]))
       subscribe_data(ws, arr[2:length(arr)])
-      # @info "The subscribe_data has been called"
     end
   end
 end
+
 include("CryptoAggregatesProcessor.jl")
+using Base.@atomic
+function pub_data(mgs_chanl::Channel)
+    # msg_out = ""
+    task_inside = @async WebSockets.listen("127.0.0.1", UInt16(8081)) do ws
+      # if msg_out != ""
+      #     send(ws, msg_out)
+      #     msg_out = ""        
+      # end
+      try
+          while true
+            @atomic begin
+              msg_out = take!(mgs_chanl)
+              send(ws, msg_out)
+            end
+              # msg_out = ""
+          end
+      catch error
+          if isa(error, Base.IOError) && error.code == -32
+              println("Connection is shut down by the client, re-establish websocket")
+              println(error)
+          else
+              # Unexpected error
+              throw(error)
+          end
+      end
+    end
+
+end
+
 function subscribe_data(ws, arr)
   for i in eachindex(arr)
       print(arr[i],"\n")
-      write(ws, JSON.json(arr[i]))
+      WebSockets.send(ws, JSON.json(arr[i]))
   end
   # count the data processing time
   counter = 0
   data_to_save = Vector{Int64}()
-  
-  while isopen(ws) 
-      received_data, success = readguarded(ws)
-      if success
+  mgs_chanl = Channel(1)
+  pub_data(mgs_chanl)
+  while isopen(ws.io) 
+      received_data = WebSockets.receive(ws)
+      
           data = String(received_data)
           time_before_processing = Dates.value(now())
-          process_websocket_data(data)
+          process_websocket_data(data, mgs_chanl)
           time_after_processing = Dates.value(now())
           print("begin: ", time_before_processing, ", after: ",time_after_processing, ", processing_time: ", time_after_processing - time_before_processing,", \n")
-          # @info "streamed data" data
           # keep track of processing times
           counter+=1
           append!(data_to_save, time_after_processing - time_before_processing)
-      end
+      
   end
   print(data_to_save)
 end
--- app/resources/cryptoaggregates/CryptoAggregatesProcessor.jl
+++ app/resources/cryptoaggregates/CryptoAggregatesProcessor.jl
@@ -1,6 +1,6 @@
-using JSON, Dates, Genie
+using JSON, Dates, Genie, HTTP
 
-function process_websocket_data(string_data::AbstractString)
+function process_websocket_data(string_data::AbstractString, mgs_chanl::Channel)
     data = JSON.parse(string_data)
     data_single = data[1]
     if "status" in keys(data_single)
@@ -24,7 +24,16 @@ function process_websocket_data(string_data::AbstractString)
     v=data_single["v"], vw=data_single["vw"],z=data_single["z"])
     time_after_store = Dates.value(now())
     save(ca_single)
+
+    # Broadcast the data to frontend
     Genie.WebChannels.broadcast("____", string_data)
+    # Send as websocket message
+    if !isempty(mgs_chanl.data)
+        take!(mgs_chanl)
+    end
+    
+    put!(mgs_chanl, JSON.json(data_single))
+
     time_after_notify_frontend = Dates.value(now())
     println("Update frontend_time: ", time_after_notify_frontend - time_after_store)
 end
